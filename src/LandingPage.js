e2c0f2dc122132334e423cdd495a50c0d0ffc9c4
Jessy Hu <jesshuy12@gmail.com>
2019-10-28 22:16:59 -0400

    first test

---
+++ src/LandingPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+class LandingPage extends React.Component {
+
+    render() {
+        return (
+            <div>Test</div>
+        );
+    }
+}
+
+export default LandingPage;
